43c55b6887eef0499ba82a2fc870c43ec81dac4a
IwantMcdo <mjhonmarcelo@gmail.com>
2025-04-26 02:35:33 +0800

    Final commit

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -32,7 +32,7 @@ load_dotenv()  # reads from .env
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['ai-recommender-k6wy.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ai-recommender-irej.onrender.com', 'localhost', '127.0.0.1']
 SECRET_KEY = os.getenv("SECRET_KEY", "fallback_dev_secret")
 OPENAI_KEY = os.getenv("OPENAI_KEY")
 MONGO_URI = os.getenv("MONGO_URI")
